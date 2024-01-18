9f3b99e261fd322e8e1343b6136f04089e7ec0e4
fakhri <fakhri.fironus@tmrnd.com.my>
2024-01-19 00:52:30 +0800

    🏷️ add color and opacity variables

--- lib/widgets/duotone_icon.dart
+++ lib/widgets/duotone_icon.dart
@@ -4,16 +4,18 @@ typedef DuotoneIconData = (IconData, IconData);
 
 class DuotoneIcon extends StatelessWidget {
   final DuotoneIconData icon;
+  final Color? color;
+  // final int? quarterTurns;
+  final double opacity;
+  // final double? percentage;
 
-  const DuotoneIcon(this.icon, {super.key});
+  const DuotoneIcon(
+      {super.key, required this.icon, this.color, this.opacity = 0.2});
 
   @override
   Widget build(BuildContext context) =>
       Stack(alignment: AlignmentDirectional.center, children: [
-        Opacity(
-          opacity: 0.2,
-          child: Icon(icon.$1, color: Colors.pink),
-        ),
-        Icon(icon.$2, color: Colors.pink),
+        Opacity(opacity: opacity, child: Icon(icon.$1, color: color)),
+        Icon(icon.$2, color: color),
       ]);
 }
