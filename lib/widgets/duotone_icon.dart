ad53ba19f5602f9c16540bb8809d1c34e8efc106
fakhri <fakhri.fironus@tmrnd.com.my>
2024-01-24 14:58:38 +0800

    ✨ add duotone icon degree variable

--- lib/widgets/duotone_icon.dart
+++ lib/widgets/duotone_icon.dart
@@ -2,19 +2,22 @@ import 'package:flutter/material.dart';
 
 typedef DuotoneIconData = (IconData, IconData);
 
+// TODO: Add filled duotone icon constructor with direction and percentage parameters.
 class DuotoneIcon extends StatelessWidget {
   final DuotoneIconData icon;
   final Color? color;
-  // final int? quarterTurns;
+  final double degree;
   final double opacity;
-  // final double? percentage;
 
-  const DuotoneIcon(this.icon, {super.key, this.color, this.opacity = 0.2});
+  const DuotoneIcon(this.icon,
+      {super.key, this.color, this.degree = 0, this.opacity = 0.2});
 
   @override
-  Widget build(BuildContext context) =>
-      Stack(alignment: AlignmentDirectional.center, children: [
-        Opacity(opacity: opacity, child: Icon(icon.$1, color: color)),
-        Icon(icon.$2, color: color),
-      ]);
+  Widget build(BuildContext context) => RotationTransition(
+        turns: AlwaysStoppedAnimation(degree / 360),
+        child: Stack(alignment: AlignmentDirectional.center, children: [
+          Opacity(opacity: opacity, child: Icon(icon.$1, color: color)),
+          Icon(icon.$2, color: color),
+        ]),
+      );
 }
