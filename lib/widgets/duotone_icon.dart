e8812b5b4f1401972b6900e127a30401ea89247c
fakhri <fakhri.fironus@tmrnd.com.my>
2024-02-01 10:45:24 +0800

    🏷️ rename degree to angle

--- lib/widgets/duotone_icon.dart
+++ lib/widgets/duotone_icon.dart
@@ -6,15 +6,15 @@ typedef DuotoneIconData = (IconData, IconData);
 class DuotoneIcon extends StatelessWidget {
   final DuotoneIconData icon;
   final Color? color;
-  final double degree;
+  final double angle;
   final double opacity;
 
   const DuotoneIcon(this.icon,
-      {super.key, this.color, this.degree = 0, this.opacity = 0.2});
+      {super.key, this.color, this.angle = 0, this.opacity = 0.2});
 
   @override
   Widget build(BuildContext context) => RotationTransition(
-        turns: AlwaysStoppedAnimation(degree / 360),
+        turns: AlwaysStoppedAnimation(angle / 360),
         child: Stack(alignment: AlignmentDirectional.center, children: [
           Opacity(opacity: opacity, child: Icon(icon.$1, color: color)),
           Icon(icon.$2, color: color),
